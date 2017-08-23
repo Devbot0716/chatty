07eb971c28af5ffb142c9ccfa9798ff6374dc057
Devbot0716 <pzepher@gmail.com>
2017-08-23 16:25:30 -0600

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
 
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+var port = 3000;
+app.listen(port);
+console.log('listening on port ' + port)
